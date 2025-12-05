2d7b9f9c80922a90de918d7c4c238c91622312d4
vinod <vky.1972@gmail.com>
2025-12-05 16:35:24 +0530

    latest 5-12-2025

--- server.js
+++ server.js
@@ -7,6 +7,33 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index');   
+});
+app.get('/contact', (req, res) => {
+  res.render('contact');   
+});
+app.get('/about-us', (req, res) => {
+  res.render('about-us');   
+});
+app.get('/faq', (req, res) => {
+  res.render('faq');   
+});
+app.get('/blogs', (req, res) => {
+  res.render('blogs');  
+});
+app.get('/signup', (req, res) => {
+  res.render('signup');  
+});
+app.post('/employer-dashboard', (req, res) => {
+  res.render('employer-dashboard');  
+});
+
 
 // Routes
 app.use("/users", userRoutes);
